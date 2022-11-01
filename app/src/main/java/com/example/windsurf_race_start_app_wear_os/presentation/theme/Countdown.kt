c84ee4743fd8267a24397c94a7b7d0e0749f0fa3
Haldun Atar <haldun.atar@recruitnow.nl>
2022-11-01 09:22:19 +0100

    add timer

--- app/src/main/java/com/example/windsurf_race_start_app_wear_os/presentation/theme/Countdown.kt
+++ app/src/main/java/com/example/windsurf_race_start_app_wear_os/presentation/theme/Countdown.kt
@@ -1,17 +1,24 @@
 package com.example.windsurf_race_start_app_wear_os.presentation.theme
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.*
+import kotlinx.coroutines.delay
+import kotlin.math.floor
 
 @Composable
 fun CountdownScreen() {
@@ -20,17 +27,40 @@ fun CountdownScreen() {
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Countdown()
-        CountdownButtons()
+//        CountdownButtons()
+        Timer()
     }
-}
-
-@Composable
-fun Countdown() {
-    Text("01:22", modifier = Modifier, Color.White, 65.sp, fontWeight = FontWeight.Bold)
 }
 
 @Composable
 fun CountdownButtons() {
     
+}
+
+@Composable
+fun Timer(
+) {
+    val countdownType = 240
+    var currentTime by remember {
+        mutableStateOf(countdownType)
+    }
+    val isTimerRunning by remember {
+        mutableStateOf(true)
+    }
+    LaunchedEffect(key1 = currentTime, key2 = isTimerRunning) {
+        if(currentTime != 0 && isTimerRunning) {
+            if (currentTime != countdownType ) delay(1000L)
+
+            currentTime -= 1
+        }
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+    ) {
+        val minutes = currentTime / 60
+        val seconds = currentTime - minutes * 60;
+
+        Text("$minutes:$seconds", modifier = Modifier, Color.White, 75.sp, fontWeight = FontWeight.Bold)
+    }
 }
